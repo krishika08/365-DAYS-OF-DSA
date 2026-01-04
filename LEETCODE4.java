ca4b66f4d61df458033e9e0058cd1a2814e707ff
KRISHIKA <krishika1400@gmail.com>
2026-01-04 21:48:53 +0530

    Contains Duplicates II

---
+++ LEETCODE4.java
@@ -0,0 +1,19 @@
+public class LEETCODE4 {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        boolean result = containsNearbyDuplicate(nums, k);
+        System.out.println(result); 
+    }
+}
