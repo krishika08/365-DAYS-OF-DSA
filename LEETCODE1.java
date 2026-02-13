b020a5cc80c381ff748a38bbd52e55e9c6d8af68
KRISHIKA <krishika1400@gmail.com>
2026-02-13 23:48:02 +0530

    Clean up whitespace in LEETCODE1.java
    Removed unnecessary whitespace in the twoSum method.

--- LEETCODE1.java
+++ LEETCODE1.java
@@ -7,12 +7,9 @@ class Solution {
                 }
             }
         }
-
-        
         return new int[] {};
     }
      public static void main(String[] args) {
-        
         int[] nums = {2, 7, 11, 15};
         int target = 9;
          Solution solution = new Solution();
