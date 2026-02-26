222e5205272180024af2f54fc0a07b823259cc7f
KRISHIKA <krishika1400@gmail.com>
2026-02-26 23:27:24 +0530

    Remove unnecessary blank lines in threeSum method

--- LEETCODE11.java
+++ LEETCODE11.java
@@ -3,8 +3,6 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-
-          
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
